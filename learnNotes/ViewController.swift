9be7efabf4ad34c06ef38bd31d7d06d407173726
Ian Bourke <ian@Ians-MacBook-Pro.local>
2017-02-17 14:10:18 -0500

    Create the table and a data source

--- learnNotes/ViewController.swift
+++ learnNotes/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    // Declarations
+    @IBOutlet weak var table: UITableView!
+    var data:[String] = ["Row 1", "Row 2", "Row 3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Notes"
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNote))
+        self.navigationItem.rightBarButtonItem = addButton
+    }
+    
+    func addNote() {
+        let name:String = "Row \(data.count + 1)"
+        data.insert(name, at: 0)
+        let indexPath:IndexPath = IndexPath(row: 0, section: 0)
+        table.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    // Required functions for UITableViewDataSource protocol
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
