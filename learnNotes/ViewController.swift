7432a05261735c48600338222ceba4c41a99aa6f
Ian Bourke <ian@Ians-MacBook-Pro.local>
2017-02-17 14:49:00 -0500

    Working with multiple ViewControllers

--- learnNotes/ViewController.swift
+++ learnNotes/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     // Declarations
     @IBOutlet weak var table: UITableView!
     var data:[String] = []
+    var selectedRow:Int = -1
+    var newRowText:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,15 +25,29 @@ class ViewController: UIViewController, UITableViewDataSource {
         load()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if selectedRow == -1 {
+            return
+        }
+        data[selectedRow] = newRowText
+        if newRowText == "" {
+                data.remove(at: selectedRow)
+        }
+        table.reloadData()
+        save()
+    }
+    
     func addNote() {
         if (table.isEditing) {
             return
         }
-        let name:String = "Row \(data.count + 1)"
+        let name:String = ""
         data.insert(name, at: 0)
         let indexPath:IndexPath = IndexPath(row: 0, section: 0)
         table.insertRows(at: [indexPath], with: .automatic)
-        save()
+        table.selectRow(at: indexPath, animated: true, scrollPosition: .none)
+        self.performSegue(withIdentifier: "detail", sender: nil)
     }
     
     // Required functions for UITableViewDataSource protocol
@@ -55,6 +71,17 @@ class ViewController: UIViewController, UITableViewDataSource {
         data.remove(at: indexPath.row)
         table.deleteRows(at: [indexPath], with: .fade)
         save()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "detail", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let detailView:DetailViewController = segue.destination as! DetailViewController
+        selectedRow = table.indexPathForSelectedRow!.row
+        detailView.masterView = self
+        detailView.setText(t: data[selectedRow])
     }
     
     func save() {
